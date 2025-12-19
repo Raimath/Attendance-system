30d07cb4ee46f596c61e537ea3b5227896ee3676
Mr.Raimath <raimath420@gmail.com>
2025-12-19 16:10:21 +0530

    Update API proxy

--- client/vite.config.js
+++ client/vite.config.js
@@ -7,7 +7,7 @@ export default defineConfig({
   server: {
     proxy: {
       '/api': {
-        target: 'http://localhost:5000',
+        target: 'https://attendance-system-server-5okt.onrender.com',
         changeOrigin: true,
       },
     }
